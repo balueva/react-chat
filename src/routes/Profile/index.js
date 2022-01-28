1a2cc7df4373f84a6629c88f18714a54840f8212
balueva <81beu@mail.ru>
2022-01-28 10:29:15 +0700

    lesson 05

--- src/routes/Profile/index.js
+++ src/routes/Profile/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { FormGroup, FormControlLabel, Checkbox } from '@mui/material';
+import { CHANGE_SHOWNAME } from '../../store/profile';
 
 export const Profile = () => {
+
+    const dispatch = useDispatch();
+
+    const showName = useSelector(state => state.showName);
+
+
+    const onChangeShowName = (event) => {
+        dispatch({ type: CHANGE_SHOWNAME, payload: event.target.checked });
+    };
+
     return (
         <div>
-            PROFILE
+            <h1>PROFILE</h1>
+            <FormGroup>
+                <FormControlLabel control={<Checkbox checked={showName} onChange={onChangeShowName} />} label='Отображать имя' />
+            </FormGroup>
         </div>
     )
 }
+
+/*
+<FormGroup>
+                <FormControlLabel control={<Checkbox checked={showName} onChange={onChangeShowName} />} label='Отображать имя' />
+            </FormGroup> */
+
+            // <input type='checkbox' checked={showName} onChange={onChangeShowName} />
---
+++ src/store/profile/actions.js
@@ -0,0 +1,1 @@
+export const CHANGE_SHOWNAME = 'CHANGE_SHOWNAME';
