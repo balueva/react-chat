25591cfd4f8d3ac983b83840a96913957f230777
balueva <81beu@mail.ru>
2022-02-02 09:06:37 +0700

    lesson 06

--- src/routes/Chats/index.js
+++ src/routes/Chats/index.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import { AUTHOR_BOT } from '../../const';
+import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import { ChatList, NewChatForm } from '../../component';
@@ -10,35 +8,26 @@ import { AppBar, Toolbar, IconButton, Box } from '@mui/material';
 import AddIcon from '@mui/icons-material/Add';
 import SearchIcon from '@mui/icons-material/Search';
 
+import { getChatList, addChatAction, deleteChatAction } from '../../store/chatList';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteMessagesByChatIdAction } from '../../store/messageList';
+
 
 export const Chats = () => {
 
-    const [messageList, setMessageList] = useState([]);
     const [visibleNewChatForm, setVisibleNewChatForm] = useState(false);
 
-    const [chatList, setChatList] = useState([
-        { id: '1', caption: 'Школа 2В', statusStr: 'Класс 2В', isGroup: true, avatar: '' },
-        { id: '2', caption: 'Школа 2В родители', statusStr: 'Класс 2В только родители', isGroup: true, avatar: 'panda.jpg' },
-        { id: '3', caption: 'Liberty school', statusStr: 'Liberty school Famili&Friends-2', isGroup: true, avatar: '' },
-        { id: '4', caption: 'Художка', statusStr: '', isGroup: true, avatar: '' },
-        { id: '5', caption: 'Лиза', statusStr: '', isGroup: false, avatar: 'pikachu.jpg' },
-        { id: '6', caption: 'Вадим', statusStr: '', isGroup: false, avatar: '' },
-        { id: '7', caption: 'Ольга', statusStr: '', isGroup: false, avatar: '' }
-    ]);
+    const chatList = useSelector(getChatList);
+    const dispatch = useDispatch();
 
     const { chatId } = useParams();
     const chat = chatId ? chatList.find(item => item.id === chatId) : null;
 
-    const addMessage = (author, text) => {
-        const newMessageList = [...messageList, { id: uuidv4(), author: author, text: text, date: new Date() }];
-        setMessageList(newMessageList);
-    };
-
     const addChat = (caption) => {
         setVisibleNewChatForm(false);
 
-        const newChatList = [...chatList, { id: uuidv4(), caption: caption, statusStr: '', isGroup: false, avatar: '' }];
-        setChatList(newChatList);
+        const newChat = { id: uuidv4(), caption: caption, statusStr: '', isGroup: false, avatar: '' };
+        dispatch(addChatAction(newChat))
     }
 
     const cancelAddChat = () => {
@@ -50,20 +39,13 @@ export const Chats = () => {
     };
 
     const deleteChat = (deletedId) => {
-        //console.log('chatId = ' + deletedId);
-        const newChatList = chatList.filter(item => item.id !== deletedId);
-        //console.log('newChatList, ' + newChatList);
-        setChatList(newChatList);
+        dispatch(deleteChatAction(deletedId));
+        dispatch(deleteMessagesByChatIdAction(deletedId));
     };
 
-    useEffect(() => {
-        const botAnswer = setTimeout(() => {
-            if (messageList.length > 0 && messageList[messageList.length - 1].author !== AUTHOR_BOT)
-                addMessage(AUTHOR_BOT, `Hello! It's a ${AUTHOR_BOT}`);
-        }, 1500);
-
-        return () => { clearTimeout(botAnswer) };
-    }, [messageList]); // eslint-disable-line react-hooks/exhaustive-deps
+    const chatArea = () => {
+        return chat ? <Chat chat={chat} /> : <div>No Chat</div>;
+    }
 
     return (
         <div className='chats'>
@@ -80,13 +62,12 @@ export const Chats = () => {
                     </Toolbar>
                 </AppBar>
                 <NewChatForm addChat={addChat} cancelAddChat={cancelAddChat} visible={visibleNewChatForm}></NewChatForm>
-                <ChatList chatList={chatList} chat={chat} deleteChat={deleteChat}></ChatList>
+                <ChatList selectedChatId={chat ? chat.id : -1} deleteChat={deleteChat}></ChatList>
             </div>
             <div className='chatarea'>
-                <Chat chat={chat} messageList={messageList} addMessage={addMessage} />
+                {chatArea()}
             </div>
         </div>
     );
-}
-
+};
 
---
+++ src/store/chatList/reducer.js
@@ -0,0 +1,21 @@
+import { ADD_CHAT, DELETE_CHAT, SET_SELECTED_CHAT } from './actions';
+
+const initialSate = {
+    chatList: [{ id: '1', caption: 'Школа 2В', statusStr: 'Класс 2В', isGroup: true, avatar: '' },
+    { id: '2', caption: 'Школа 2В родители', statusStr: 'Класс 2В только родители', isGroup: true, avatar: 'panda.jpg' },
+    { id: '3', caption: 'Liberty school', statusStr: 'Liberty school Famili&Friends-2', isGroup: true, avatar: '' },
+    { id: '4', caption: 'Художка', statusStr: '', isGroup: true, avatar: '' },
+    { id: '5', caption: 'Лиза', statusStr: '', isGroup: false, avatar: 'pikachu.jpg' },
+    { id: '6', caption: 'Вадим', statusStr: '', isGroup: false, avatar: '' },
+    { id: '7', caption: 'Ольга', statusStr: '', isGroup: false, avatar: '' }]
+};
+
+export const chatListReducer = (state = initialSate, action) => {
+    switch (action.type) {
+        case ADD_CHAT:
+            return { ...state, chatList: [...state.chatList, action.payload] }
+        case DELETE_CHAT:
+            return { ...state, chatList: state.chatList.filter(item => item.id !== action.payload) }
+        default: return state;
+    }
+};
